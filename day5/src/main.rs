869f64755ef80b1ddfc8261ff234ee907b61b19f
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-05 08:00:31 +0000

    Created solution for day5 (was placeholder before)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,111 +1,88 @@
-use std::ops::Index;
-
-use diagonal::{diagonal_pos_neg, diagonal_pos_pos, straight_y};
+use std::{cmp::Ordering, collections::HashSet};
 
 fn main() {
+    let mut rules: HashSet<(i32, i32)> = HashSet::new();
+    let mut updates = vec![];
+    let mut ans;
+
     let input = std::fs::read_to_string("example.txt").unwrap();
-    println!("Value for part 1 is:\n{}", part1(&input));
-    println!("Value for part 2 is:\n{}", part2(&input));
+    parse(&input, &mut updates, &mut rules);
+    ans = solve(&updates, &rules);
+    println!("Answer for example is: {}, {}", ans.0, ans.1);
 
     let input = std::fs::read_to_string("input.txt").unwrap();
-    println!("Value for part 1 is:\n{}", part1(&input));
-    println!("Value for part 2 is:\n{}", part2(&input));
+    parse(&input, &mut updates, &mut rules);
+    ans = solve(&updates, &rules);
+    println!("Answer is: {}, {}", ans.0, ans.1);
 }
 
-fn part1(input: &str) -> usize {
+fn solve(updates: &[Vec<i32>], rules: &HashSet<(i32, i32)>) -> (i32, i32) {
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    for update in updates {
+        if is_valid(&update, &rules) {
+            p1 += update[(update.len() - 1) / 2];
+        } else {
+            p2 += part2(update, rules);
+        }
+    }
+
+    return (p1, p2);
+}
+
+fn part2(invalid_update: &[i32], rules: &HashSet<(i32, i32)>) -> i32 {
     let mut val = 0;
-    let mut grid: Vec<Vec<char>> = vec![];
+    let mut sorted = invalid_update.to_vec();
 
-    for line in input.lines() {
-        val += line.matches("XMAS").count();
-        val += line.matches("SAMX").count();
+    sorted.sort_by(|a, b| {
+        if rules.contains(&(*a, *b)) {
+            return Ordering::Less;
+        }
 
-        grid.push(line.chars().collect());
-    }
+        Ordering::Greater
+    });
 
-    let bars = straight_y(&grid);
-
-    for line in bars {
-        let s: String = line.iter().map(|c| **c).collect();
-        val += s.matches("XMAS").count();
-        val += s.matches("SAMX").count();
-    }
-
-    // 1 2 3 4
-    // 2 x x x
-    // 3 x x x
-    // 4 x x x
-    // 5 x x x
-    let diag1 = diagonal_pos_pos(&grid);
-    let diag2 = diagonal_pos_neg(&grid);
-
-    for line in diag1 {
-        let s: String = line.iter().map(|c| **c).collect();
-        val += s.matches("XMAS").count();
-        val += s.matches("SAMX").count();
-    }
-    for line in diag2 {
-        let s: String = line.iter().map(|c| **c).collect();
-        val += s.matches("XMAS").count();
-        val += s.matches("SAMX").count();
-    }
+    val += sorted[(sorted.len() - 1) / 2];
 
     return val;
 }
 
-//println!("X-MAS at {col}, {row}");
-fn part2(input: &str) -> usize {
-    let mut val = 0;
+// checks if a given update is valid
+fn is_valid(update: &Vec<i32>, rules: &HashSet<(i32, i32)>) -> bool {
+    for (i1, p1) in update.iter().enumerate() {
+        // takes index of all rules that involve the number as its first arg
+        for (_, b) in rules.iter().filter(|r| r.0 == *p1) {
+            if let Some(i2) = update.iter().position(|n| *n == *b) {
+                if i2 < i1 {
+                    return false;
+                }
+            }
+        }
+    }
 
-    let mut grid: Vec<Vec<char>> = vec![];
+    true
+}
+
+// parses input into updates and rules
+fn parse(input: &str, updates: &mut Vec<Vec<i32>>, rules: &mut HashSet<(i32, i32)>) {
+    rules.clear();
+    updates.clear();
 
     for line in input.lines() {
-        grid.push(line.chars().collect());
-    }
-
-    // M.M     S.M
-    // .A.     .A.
-    // S.S     S.M
-
-    // always a buffer for A
-    // skip the first line
-    let col_size = grid.len();
-    let row_size = grid[0].len();
-
-    for (mut col, line) in grid.iter().skip(1).enumerate() {
-        col += 1; // since skipped first
-        if col >= col_size - 1 {
-            // skip last
-            break;
-        }
-        for (mut row, c) in line.iter().skip(1).enumerate() {
-            row += 1; // since skipped first
-            if row >= row_size - 1 {
-                // skip last
-                break;
+        // takes note of all the rules
+        if line.contains('|') {
+            let mut i = line.split('|');
+            rules.insert((
+                i.next().unwrap().parse().unwrap(),
+                i.next().unwrap().parse().unwrap(),
+            ));
+            if i.next().is_some() {
+                panic!()
             }
-
-            if *c != 'A' {
-                continue;
-            }
-
-            // matches:
-            // A B
-            // C D
-            match (
-                grid[col - 1].index(row - 1),
-                grid[col - 1].index(row + 1),
-                grid[col + 1].index(row - 1),
-                grid[col + 1].index(row + 1),
-            ) {
-                ('M', 'M', 'S', 'S')
-                | ('M', 'S', 'M', 'S')
-                | ('S', 'M', 'S', 'M')
-                | ('S', 'S', 'M', 'M') => val += 1,
-                _ => { /*  do nothing */ }
-            }
+        } else if line.contains(',') {
+            // get the numbers in a line
+            updates.push(line.split(',').map(|c| c.parse::<i32>().unwrap()).collect());
         }
     }
-
-    return val;
 }
