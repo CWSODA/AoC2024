b3f51d4eb8827fe122700d3bc7ff7d03652c415d
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-06 15:53:45 +0000

    Clean up day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let input = std::fs::read_to_string("example.txt").unwrap();
-    println!("Value for part 1 is:\n{}", part1(&input));
+    println!("Example value for part 1:\n{}", part1(&input));
 
     let input = std::fs::read_to_string("example2.txt").unwrap();
-    println!("Value for part 2 is:\n{}", part2(&input));
+    println!("Example value for part 2:\n{}", part2(&input));
 
     let input = std::fs::read_to_string("input.txt").unwrap();
-    println!("Value for part 1 is:\n{}", part1(&input));
-    println!("Value for part 2 is:\n{}", part2(&input));
+    println!("Value for part 1:\n{}", part1(&input));
+    println!("Value for part 2:\n{}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
@@ -20,45 +20,45 @@ fn part1(input: &str) -> i32 {
             None => continue, // if no closing bracket then invalid
         };
 
-        if slice.contains(|e: char| e != ',' && !e.is_numeric()) {
-            continue; // if contains something other than comma or number, invalid
-        }
-
-        if slice.chars().filter(|c| *c == ',').count() != 1 {
-            continue; // if not exactly one comma, invalid
+        // ensures slice only contains one comma and numbers
+        let mut comma_count = 0;
+        if !slice.chars().all(|c| {
+            if c == ',' {
+                comma_count += 1;
+                true
+            } else {
+                c.is_numeric()
+            }
+        }) || comma_count != 1
+        {
+            continue;
         }
 
         // thus there should only be numbers left
-        let nums = slice.split(',');
-        if nums.clone().count() != 2 {
+        let nums: Vec<&str> = slice.split(',').collect();
+        if nums.len() != 2 {
             continue; // not enough numbers, invalid
         }
 
-        let mut product = 1;
-        for num in nums {
-            product *= num.parse::<i32>().unwrap();
-        }
-
-        val += product;
+        val += nums[0].parse::<i32>().unwrap() * nums[1].parse::<i32>().unwrap();
     }
 
-    // println!("Value for part 1 is:\n{val}");
-    return val;
+    val
 }
 
 fn part2(input: &str) -> i32 {
-    let mut val = 0;
-
     // takes everything in between do()
-    // note do is enabled by default so first part is still valid
-    for parts in input.split("do()") {
-        val += part1(parts.split("don't()").next().unwrap());
-    }
+    // note: do is enabled by default so first part is still valid
+    input
+        .split("do()")
+        .map(|part| part1(part.split("don't()").next().unwrap()))
+        .sum()
 
     // xmul(2,4)&mul[3,7]!^  don't()  _mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
     // xmul(2,4)&mul[3,7]!^  don't()  _mul(5,5)+mul(32,64](mul(11,8)un
     // ?mul(8,5))
     // only take first slice before don't
-
-    return val;
 }
+
+// 161, 48
+// 173419328, 90669332
