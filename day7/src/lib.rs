2e9c812f07e6602efbf3bb85e5939d947cbc90bd
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-07 15:22:35 +0000

    Precompiled combinations

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,6 +1,8 @@
-use std::iter::repeat_n;
-
 use itertools::Itertools;
+use std::{
+    collections::{HashMap, HashSet},
+    iter::repeat_n,
+};
 
 pub fn solve(input: &str) -> (u64, u64) {
     let equations = parse(input);
@@ -11,9 +13,25 @@ pub fn solve(input: &str) -> (u64, u64) {
 fn part1(equations: &[Vec<u64>]) -> u64 {
     let mut out = 0;
 
+    let mut eq_lengths = HashSet::new();
+    for eq in equations {
+        eq_lengths.insert(eq.len());
+    }
+    let combs: HashMap<usize, Vec<Vec<OP>>> = eq_lengths
+        .into_iter()
+        .map(|len| {
+            (
+                len,
+                repeat_n([OP::ADD, OP::MULT], len - 2)
+                    .multi_cartesian_product()
+                    .collect(),
+            )
+        })
+        .collect();
+
     for eq in equations {
         // all operator permutations with replacement
-        'comb: for comb in repeat_n([OP::MULT, OP::ADD], eq.len() - 2).multi_cartesian_product() {
+        for comb in combs.get(&eq.len()).expect("Invalid equation length") {
             // assign first number to val
             let mut val = eq[1];
 
@@ -26,7 +44,7 @@ fn part1(equations: &[Vec<u64>]) -> u64 {
 
                 // val only increases with each operation
                 if val > eq[0] {
-                    continue 'comb;
+                    break;
                 }
             }
 
@@ -41,6 +59,88 @@ fn part1(equations: &[Vec<u64>]) -> u64 {
     out
 }
 
+fn part2(equations: &[Vec<u64>]) -> u64 {
+    let mut out = 0;
+
+    let mut eq_lengths = HashSet::new();
+    for eq in equations {
+        eq_lengths.insert(eq.len());
+    }
+    let combs: HashMap<usize, Vec<Vec<OP>>> = eq_lengths
+        .into_iter()
+        .map(|len| {
+            (
+                len,
+                repeat_n([OP::ADD, OP::MULT, OP::CONC], len - 2)
+                    .multi_cartesian_product()
+                    .collect(),
+            )
+        })
+        .collect();
+
+    for eq in equations {
+        // all operator permutations with replacement
+        let combs = combs.get(&eq.len()).expect("Invaid equation length");
+
+        'comb_test: for comb in combs {
+            // assign first number to val
+            let mut val = eq[1];
+
+            for (num, op) in eq.into_iter().skip(2).zip_eq(comb) {
+                match op {
+                    OP::MULT => val *= num,
+                    OP::ADD => val += num,
+                    OP::CONC => val = concact_num(val, *num),
+                }
+
+                // val only increases with each operation so stop current comb test
+                if val > eq[0] {
+                    continue 'comb_test;
+                }
+            }
+
+            if val == eq[0] {
+                // valid operation, add output number
+                out += val;
+                break;
+            }
+        }
+    }
+
+    out
+}
+
+fn parse(input: &str) -> Vec<Vec<u64>> {
+    let mut equations = vec![];
+    for line in input.lines() {
+        equations.push(
+            line.split(|c: char| !c.is_numeric())
+                .filter(|c| !c.is_empty())
+                .map(|c| c.parse().expect("Error parsing number"))
+                .collect(),
+        );
+    }
+
+    equations
+}
+
+fn concact_num(n1: u64, n2: u64) -> u64 {
+    n1 * 10u64.pow(n2.to_string().len() as u32) + n2
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum OP {
+    MULT,
+    ADD,
+    CONC,
+}
+
+// 3749, 11387
+// 5702958180383, 92612386119138
+//92612386119138
+//30383733113065
+
+/*
 fn part2(equations: &[Vec<u64>]) -> u64 {
     let mut out = 0;
 
@@ -75,33 +175,4 @@ fn part2(equations: &[Vec<u64>]) -> u64 {
 
     out
 }
-
-fn parse(input: &str) -> Vec<Vec<u64>> {
-    let mut equations = vec![];
-    for line in input.lines() {
-        equations.push(
-            line.split(|c: char| !c.is_numeric())
-                .filter(|c| !c.is_empty())
-                .map(|c| c.parse().expect("Error parsing number"))
-                .collect(),
-        );
-    }
-
-    equations
-}
-
-fn concact_num(n1: u64, n2: u64) -> u64 {
-    format!("{n1}{n2}")
-        .parse()
-        .expect("unable to parse contacted number")
-}
-
-#[derive(Clone, Copy)]
-enum OP {
-    MULT,
-    ADD,
-    CONC,
-}
-
-// 3749, 11387
-// 5702958180383, 92612386119138
+*/
