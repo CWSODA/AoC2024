6967ac5f1cf02de805c4615f03cf2ca46cdd3432
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-08 07:43:40 +0000

    Renamed day_n  to day_0n so that they show up in order. Hopefully I didn't break anything.

---
+++ day06/src/main.rs
@@ -0,0 +1,14 @@
+use day06::solve;
+
+// ngl i have no idea what number is best
+const THREAD_COUNT: usize = 16;
+
+fn main() {
+    let input = std::fs::read_to_string("example.txt").unwrap();
+    let ans = solve(&input, THREAD_COUNT);
+    println!("Answer for example is: {}, {}", ans.0, ans.1);
+
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let ans = solve(&input, THREAD_COUNT);
+    println!("Answer is: {}, {}", ans.0, ans.1);
+}
