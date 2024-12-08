22cc722f4522cc744c1ad3775b7e1fe282dc55b9
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-08 06:21:39 +0000

    Added placeholders for future days

---
+++ day13/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn solve(input: &str) -> (usize, usize) {
+    (part1(&points, grid_size), part2(&points, grid_size))
+}
+
+fn part1() -> usize {
+    0
+}
+
+fn part2() -> usize {
+    0
+}
+
+fn parse(input: &str) -> () {}
