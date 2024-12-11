c65c57f8b2cee30cc0ae0ad5b1820ad8e7205f1f
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-11 10:02:10 +0000

    Solution to day11

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn solve(input: &str) -> (usize, usize) {
-    parse(input);
-    (part1(), part2())
+use hashbrown::HashMap;
+
+pub fn solve(input: &str, blinks: usize) -> usize {
+    // keep track of previously seen
+    // (stone, blinks_left), count after blinks
+    let mut seen: HashMap<(u64, usize), usize> = HashMap::new();
+
+    input
+        .split_whitespace()
+        .map(|num| stoning(num.parse().unwrap(), blinks, &mut seen))
+        .sum()
 }
 
-fn part1() -> usize {
-    0
-}
+fn stoning(stone: u64, blinks: usize, seen: &mut HashMap<(u64, usize), usize>) -> usize {
+    if blinks == 0 {
+        return 1;
+    } else if let Some(cached) = seen.get(&(stone, blinks)) {
+        return *cached;
+    }
 
-fn part2() -> usize {
-    0
-}
+    let val = if stone == 0 {
+        stoning(1, blinks - 1, seen)
+    } else {
+        let digits = stone.ilog10() + 1;
+        if digits % 2 == 0 {
+            let mask = 10u64.pow(digits / 2);
+            stoning(stone / mask, blinks - 1, seen) + stoning(stone % mask, blinks - 1, seen)
+        } else {
+            stoning(stone * 2024, blinks - 1, seen)
+        }
+    };
 
-fn parse(input: &str) -> () {}
+    seen.insert((stone, blinks), val);
+    val
+}
