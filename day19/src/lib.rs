161e0e1d6f06c95383011fb976f07e7b888f8fee
CWSODA <76740111+CWSODA@users.noreply.github.com>
2024-12-19 06:25:40 +0000

    Initial solution to day19

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,14 +1,46 @@
+use hashbrown::HashMap;
+
 pub fn solve(input: &str) -> (usize, usize) {
-    parse(input);
-    (part1(), part2())
+    let (towels, designs) = parse(input);
+
+    let mut possible_designs = 0;
+    let mut combinations = 0;
+
+    let mut memo = HashMap::new();
+    for design in designs.lines() {
+        let n = check(&towels, design, &mut memo);
+        if n > 0 {
+            possible_designs += 1;
+            combinations += n;
+        }
+    }
+
+    (possible_designs, combinations)
 }
 
-fn part1() -> usize {
-    0
+fn check<'a>(towels: &[&str], design: &'a str, memo: &mut HashMap<&'a str, usize>) -> usize {
+    if design.is_empty() {
+        return 1;
+    }
+
+    if let Some(n) = memo.get(design) {
+        return *n;
+    }
+
+    let n = towels
+        .iter()
+        .filter(|t| design.starts_with(*t))
+        .map(|t| check(towels, &design[t.len()..], memo))
+        .sum();
+
+    memo.insert(design, n);
+    n
 }
 
-fn part2() -> usize {
-    0
-}
+fn parse(input: &str) -> (Vec<&str>, &str) {
+    let (a, designs) = input.split_once("\n\n").expect("Invalid format");
 
-fn parse(input: &str) -> () {}
+    let towels = a.split(", ").collect();
+
+    (towels, designs)
+}
--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,4 +1,4 @@
-use day9::solve;
+use day19::solve;
 
 fn main() {
     let input = std::fs::read_to_string("example.txt").unwrap();
@@ -9,3 +9,8 @@ fn main() {
     let ans = solve(&input);
     println!("Answer is: {}, {}", ans.0, ans.1);
 }
+
+/*
+Answer for example is: 6, 16
+Answer is: 251, 616957151871345
+*/
